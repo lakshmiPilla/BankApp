c2340d9748740b71fa8a336ced5f47606eba4e6c
Lakshmi Pilla <lakshmi_pilla@live.com>
2017-12-06 22:58:02 -0800

    initial commit of Bank app

---
+++ BankApp/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Account
+    {
+        #region
+        public int AccountNumber { get; set; }
+        public string Emailaddress { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        #endregion
+    }
+}
