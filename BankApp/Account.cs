2e2b274f56d9c925946ca8131a38076a88ddb85e
Lakshmi Pilla <lakshmi_pilla@live.com>
2017-12-07 18:01:50 -0800

    static, constructor,enum,object initialiastion, overloading

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -6,14 +6,54 @@ using System.Threading.Tasks;
 
 namespace BankApp
 {
+    enum TypeOfAccount
+    {
+        Checking,
+        Savings,
+        CD,
+        Loan
+    }
     class Account
     {
-        #region
-        public int AccountNumber { get; set; }
+        private static int lastAccountNumber = 0;
+        #region Constructor
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+        //when we are calling 2nd constructor, THIS keyword calls other Constructer also
+        //public Account(string emailaddress): this()
+        //{
+        //    Emailaddress = emailaddress;
+        //}
+        //public Account(string checkingType,string emailaddress):this()
+        //{
+
+        //}
+        #endregion
+        
+        #region Properties
+        public int AccountNumber { get; private set; }
         public string Emailaddress { get; set; }
         public string AccountName { get; set; }
-        public string AccountType { get; set; }
-        public decimal Balance { get; set; }
+        public TypeOfAccount AccountType { get; set; }
+        public decimal Balance { get; private set; }
         #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">amount to be deposited</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+#endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,7 +10,30 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-          
+
+            //refernce, object, instance of a class
+            var account = new Account
+            {
+                AccountName = "MyChecking",
+                AccountType = TypeOfAccount.Checking,
+                Emailaddress = "test@test.com"
+            };
+           
+            account.Deposit(100.10M);
+            Console.WriteLine($"Account number:{account.AccountNumber}");
+            Console.WriteLine($"Email Address:{account.Emailaddress}");
+            Console.WriteLine($"Account type: {account.AccountType}");
+
+            var account2 = new Account
+            {
+                Emailaddress = "test2@test.com"
+        };
+           
+            account2.AccountType = TypeOfAccount.Savings;
+            account2.Deposit(100.10M);
+            Console.WriteLine($"Account number:{account2.AccountNumber}+" +
+                $"Email Address:{account2.Emailaddress},Account type: {account2.AccountType}");
+        } 
     }
 }
-}
+
