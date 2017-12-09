69a66ab9364eae917ecff8f37cfff00afe1cb31d
Lakshmi Pilla <lakshmi_pilla@live.com>
2017-12-08 20:27:49 -0800

    Creating menu

---
+++ BankApp/Bank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    static class Bank
+    {
+        private static List<Account> accounts = new List<Account>(); // accounts-refernce
+        public static Account CreateAccount(string emailAddress,string accountName = "Default Account", TypeOfAccount accountType = TypeOfAccount.Checking)
+        {
+            var account = new Account
+            {
+                Emailaddress = emailAddress,
+                AccountName = accountName,
+                AccountType = accountType
+            };
+            accounts.Add(account);
+            return account;
+        }
+    }
+}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,29 +10,63 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
+            //    var account =Bank.CreateAccount("test2test.com",accountType:TypeOfAccount.Savings,accountName: "MySavings");
+            //   // var account=Bank.CreateAccount("test@test.com", "MyChecking", TypeOfAccount.Checking);
+            //    //refernce, object, instance of a class
 
-            //refernce, object, instance of a class
-            var account = new Account
-            {
-                AccountName = "MyChecking",
-                AccountType = TypeOfAccount.Checking,
-                Emailaddress = "test@test.com"
-            };
-           
-            account.Deposit(100.10M);
-            Console.WriteLine($"Account number:{account.AccountNumber}");
-            Console.WriteLine($"Email Address:{account.Emailaddress}");
-            Console.WriteLine($"Account type: {account.AccountType}");
+            //    account.Deposit(100.10M);
+            //    Console.WriteLine($"Account number:{account.AccountNumber}");
+            //    Console.WriteLine($"Email Address:{account.Emailaddress}");
+            //    Console.WriteLine($"Account type: {account.AccountType}");
 
-            var account2 = new Account
+            //    var account2 = new Account
+            //    {
+            //        Emailaddress = "test2@test.com"
+            //};
+
+            //    account2.AccountType = TypeOfAccount.Savings;
+            //    account2.Deposit(100.10M);
+            //    Console.WriteLine($"Account number:{account2.AccountNumber}+" +
+            //        $"Email Address:{account2.Emailaddress},Account type: {account2.AccountType}");
+
+            Console.WriteLine("********************************************");
+            Console.WriteLine("Welcome to Bank");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("0.Exit");
+            Console.WriteLine("1.Create an account");
+            Console.WriteLine("2.Deposit");
+            Console.WriteLine("3.Withdraw");
+            Console.WriteLine("4.Print all accounts");
+
+            Console.WriteLine("Please select an option");
+            var choice = Console.ReadLine();
+
+            switch (choice)
             {
-                Emailaddress = "test2@test.com"
-        };
+                case "0":
+                    Console.WriteLine("Thank you for visiting");
+                    return;
            
-            account2.AccountType = TypeOfAccount.Savings;
-            account2.Deposit(100.10M);
-            Console.WriteLine($"Account number:{account2.AccountNumber}+" +
-                $"Email Address:{account2.Emailaddress},Account type: {account2.AccountType}");
+                case "1":
+                    Console.Write("Email address:");
+                    var emailAddress=Console.ReadLine();
+                    Console.Write("Account Name:");
+                    var accountName = Console.ReadLine();
+                    var typeofAccounts = Enum.GetNames(typeof(TypeOfAccount));
+                    
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    break;
+            }
+
+
         } 
     }
 }
